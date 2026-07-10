4ee04986a366672bb120577f56184ee8e69609be
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-07-10 20:07:49 -0300

    remove unused database DDL commands and extract function

--- etl/extract.py
+++
@@ -1,9 +0,0 @@
-def extract():
-    try:
-        conn = get_connection()
-
-        if (conn.is_connected()):
-            print('connection stabilized!')
-
-    except Exception as e:
-        print(f'ETL failed: {e}')
