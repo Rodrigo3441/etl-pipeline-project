98cacbeab43b10c649424da9f14a66c73ef5af70
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-07-13 21:43:53 -0300

    feat: add DDL for crm_prd_info table in Silver layer

--- scripts/silver/define_tables.py
+++ scripts/silver/define_tables.py
@@ -29,8 +29,21 @@ cust_info_ddl = """
                 dwh_create_date    DATETIME2 DEFAULT GETDATE()
 """
 
+prd_info_ddl = """
+                prd_id          INT,
+                cat_id          NVARCHAR(50),
+                prd_key         NVARCHAR(50),
+                prd_nm		    NVARCHAR(50),
+                prd_cost	    INT,
+                prd_line		NVARCHAR(50),
+                prd_start_dt	DATE,
+                prd_end_dt	    DATE,
+                dwh_create_date DATETIME2 DEFAULT GETDATE()
+"""
+
 tables = {
-    'crm_cust_info': cust_info_ddl
+    'crm_cust_info': cust_info_ddl,
+    'crm_prd_info': prd_info_ddl
 }
 
 def execute(engine):
