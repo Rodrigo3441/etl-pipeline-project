45e39989c2f15b51e499af720a3da02f485f0370
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-07-14 22:39:37 -0300

    add data type dictionary to standardizing

--- scripts/silver/transformations/crm_sales_details.py
+++ scripts/silver/transformations/crm_sales_details.py
@@ -1,7 +1,23 @@
 import pandas as pd
+import sys
 
 def execute(df):
 
-    
+    # data type dictionary standardization
+    df = df.astype(
+        {
+            'sls_ord_num'    : 'string',
+            'sls_prd_key'    : 'string',
+            'sls_cust_id'    : 'int32[pyarrow]',
+            'sls_order_dt'   : 'int32[pyarrow]',
+            'sls_ship_dt'    : 'int32[pyarrow]',
+            'sls_due_dt'     : 'int32[pyarrow]',
+            'sls_sales'      : 'int32[pyarrow]',
+            'sls_quantity'   : 'int32[pyarrow]',
+            'sls_price'      : 'int32[pyarrow]',
+        }
+    )
 
+    print(df)
+    sys.exit()
     return df
