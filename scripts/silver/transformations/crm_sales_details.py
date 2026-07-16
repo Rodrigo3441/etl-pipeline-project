ed09a6401ab866aa699a6f6f93a7f38d0d949bed
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-07-15 23:03:53 -0300

    add transformations for sales_details table

--- scripts/silver/transformations/crm_sales_details.py
+++ scripts/silver/transformations/crm_sales_details.py
@@ -1,7 +1,24 @@
-import pandas as pd
-import sys
+"""
+=====================================================
+Transform CRM Sales Details
+=====================================================
+Script Purpose:
+    This script applies the data cleansing and transformation rules for the
+    'crm_sales_details' table before loading the data into the Silver layer.
+    The transformations improve data quality by standardizing data types,
+    converting date columns, and correcting invalid sales and price values.
 
-def execute(df):
+Notes:
+    - Standardizes column data types.
+    - Converts integer-based dates into datetime format.
+    - Recalculates invalid sales amounts using quantity and price.
+    - Derives missing or invalid product prices from sales and quantity.
+"""
+
+import pandas as pd
+import numpy as np
+
+def execute(df: pd.DataFrame) -> pd.DataFrame:
 
     # data type dictionary standardization
     df = df.astype(
@@ -18,6 +35,49 @@ def execute(df):
         }
     )
 
-    print(df)
-    sys.exit()
+    """
+    =====================================================
+    Columns: sls_order_dt, sls_ship_dt, sls_due_dt
+    =====================================================
+    """ 
+    # convert all dates from int format to a proper date format
+    df['sls_order_dt'] = pd.to_datetime(df['sls_order_dt'], errors='coerce', format='%Y%m%d')
+    df['sls_ship_dt'] = pd.to_datetime(df['sls_ship_dt'], errors='coerce', format='%Y%m%d')
+    df['sls_due_dt'] = pd.to_datetime(df['sls_due_dt'], errors='coerce', format='%Y%m%d')
+    
+    """
+    =====================================================
+    Column: sls_quantity
+    =====================================================
+    """ 
+    # fix data quality issues related to sls_sales column and reevaluate the calculus if necessary
+    df['sls_sales'] = df['sls_sales'].case_when(
+        caselist=[
+            (
+                (df['sls_sales'].isna()) | 
+                (df['sls_sales'] <= 0) | 
+                (df['sls_sales'] != df['sls_quantity'] * df['sls_price'].abs()), 
+                (df['sls_quantity'] * df['sls_price'].abs()) # replace sls_sales with quantity * price columns
+            )
+        ]
+    )
+
+    """
+    =====================================================
+    Column: sls_price
+    =====================================================
+    """ 
+    #  derive price if original value is invalid
+    df['sls_price'] = df['sls_price'].case_when(
+        caselist=[
+            (
+                (df['sls_price'].isna()) |
+                (df['sls_price'] <= 0),
+                (df['sls_sales'] / df['sls_quantity'])
+                    .where(df['sls_quantity'] != 0, np.nan)
+                    .astype('int32[pyarrow]')
+            )
+        ]
+    )
+
     return df
