330e1ad07c6a6d277ca85492b43a67593b34cc31
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-07-27 19:37:06 -0300

    Refactor execute function in main.py to improve logging

--- main.py
+++ main.py
@@ -27,38 +27,56 @@ import scripts.gold.main_gold as gold
 import logging
 import time
 
-def execute():
+# logging configuration
+logging.basicConfig(
+                    filename='pipeline.log', 
+                    level=logging.INFO,
+                    format='%(asctime)s | %(levelname)-8s | %(filename)s:%(lineno)d | %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                    )
 
+logger = logging.getLogger()
+
+def execute():
+    logger.info('=====================================================')
+    logger.info('Starting the ETL Pipeline')
+    logger.info('=====================================================')
+
+    # variable to store the current layer that's being executed
     current_layer = ''
+
+    # variables to track time
+    bronze_time = 0
+    silver_time = 0
+    gold_time = 0
+    total_time = 0
 
     try:
         start_time = time.perf_counter()
 
-        logging.basicConfig(filename='pipeline.log', level=logging.INFO)
-        logger = logging.getLogger()
-
         current_layer = 'bronze'
+        logger.info('Executing Bronze Layer')
         bronze_time = bronze.execute()
         
         current_layer = 'silver'
+        logger.info('Executing Silver Layer')
         silver_time = silver.execute()
 
         current_layer = 'gold'
+        logger.info('Executing Gold Layer')
         gold_time = gold.execute()
 
         end_time = time.perf_counter()
 
         total_time = end_time - start_time
 
-        print(total_time)
-
     except Exception as e:
-        logger.error('=====================================================')
-        logger.error('An error occurred while executing the pipeline:')
-        logger.error('=====================================================')
-        logger.error(f'layer: {current_layer}')
-        logger.error(f'Error: {e}')
-        logger.error('Stopping the pipeline')
+        logger.exception('=====================================================')
+        logger.exception('An error occurred while executing the pipeline:')
+        logger.exception('=====================================================')
+        logger.exception(f'layer: {current_layer}')
+        logger.exception(f'Error: {e}')
+        logger.exception('Stopping the pipeline')
 
     finally:
         logger.info('===========================================================')
@@ -66,9 +84,10 @@ def execute():
         logger.info('===========================================================')
         logger.info(f'{'Total time':.<40} {total_time:.2f} sec')
         logger.info(f'{'Bronze layer':.<40} {bronze_time:.2f} sec')
-        logger.info(f'{'Silver time':.<40} {silver_time:.2f} sec')
-        logger.info(f'{'Gold time':.<40} {gold_time:.2f} sec')
+        logger.info(f'{'Silver layer':.<40} {silver_time:.2f} sec')
+        logger.info(f'{'Gold layer':.<40} {gold_time:.2f} sec')
         logger.info('===========================================================')
+        logger.info('Pipeline completed successfully.')
 
 
 if __name__ == '__main__':
